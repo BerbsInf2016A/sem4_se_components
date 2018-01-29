e590e404c67693f309a6c3cce19b0cdf34ceb06f
Robert Zebec <zebec.robert@gmail.com>
2018-01-29 11:16:19 +0100

    Implemented skeleton for sha256hash

--- sha256hash/src/Hash.java
+++ sha256hash/src/Hash.java
@@ -1,2 +1,40 @@
+import java.lang.reflect.Method;
+
 public class Hash {
+    private static Hash instance = new Hash();
+    public Port port;
+
+    private Hash(){
+        this.port = new Port();
+    }
+
+    public class Port implements IHash {
+
+        private Method[] methods = getClass().getMethods();
+
+        public String getVersion() {
+            return innergetVersion();
+        }
+
+        public String hash(String hash) {
+            return null;
+        }
+
+        public void listMethods() {
+            System.out.println("--- public methods for " + getClass().getName());
+            for (int i = 0; i < methods.length; i++)
+                if (!methods[i].toString().contains("Object") && !methods[i].toString().contains("list"))
+                    System.out.println(methods[i]);
+            System.out.println("---");
+        }
+    }
+    public String innergetVersion() {
+        return "MD5Hash - Version 1.0";
+    }
+
+    private String innerHash(String value){
+
+        // TODO: Add Function
+        return "";
+    }
 }
